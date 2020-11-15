d2551f230446c5031d6ac5853a5ac65b3b916fde
mhshadab <shadab@ualberta.ca>
2020-11-15 16:44:14 -0700

    Created Board class and added getter method

--- py_version/oominimax.py
+++ py_version/oominimax.py
@@ -4,4 +4,22 @@ from random import seed as randomseed       # Paul Lu
 import platform
 import time
 from os import system
+"""
+Muhtasim Haque Shadab
+CCID:  shadab
+"""
 
+HUMAN = -1
+COMP = +1
+
+class Board:
+    
+    def __init__(self):
+        self._board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            ]
+
+    def get_board(self):
+        return self._board
