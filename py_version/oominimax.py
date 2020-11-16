aa43b1853637cbe6f33899d2488c844992b27d28
mhshadab <shadab@ualberta.ca>
2020-11-16 12:51:01 -0700

    Added comments and did final tweaking

--- py_version/oominimax.py
+++ py_version/oominimax.py
@@ -15,6 +15,7 @@ COMP = +1
 class Board:
     
     def __init__(self):
+        self.type = str(self.__class__)
         self._board = [
             [0, 0, 0],
             [0, 0, 0],
@@ -35,6 +36,10 @@ class Board:
 
 
     def empty_cells(self):
+        """
+        Each empty cell will be added into cells' list
+        :return: a list of empty cells
+        """
         cells = []
 
         for x, row in enumerate(self.get_board()):
@@ -45,12 +50,24 @@ class Board:
         return cells
 
     def valid_move(self,x,y):
+        """
+        A move is valid if the chosen cell is empty
+        :param x: X coordinate
+        :param y: Y coordinate
+        :return: True if the board[x][y] is empty
+        """
         if [x,y] in self.empty_cells():
             return True
         else:
             return False
 
     def set_move(self,x, y, player):
+        """
+        Set the move on board, if the coordinates are valid
+        :param x: X coordinate
+        :param y: Y coordinate
+        :param player: the current player
+        """
         if self.valid_move(x, y):
             self._board[x][y] = player
             return True
@@ -60,6 +77,7 @@ class Board:
 class State:
     
     def __init__(self, board):
+        self.type = str(self.__class__)
         self.board = board
 
     def __str__(self):
@@ -69,6 +87,14 @@ class State:
         s = "<%d> %s" % (id(self), self.type)
         return(s)
     def wins(self, player):
+        """
+        This function tests if a specific player wins. Possibilities:
+        * Three rows    [X X X] or [O O O]
+        * Three cols    [X X X] or [O O O]
+        * Two diagonals [X X X] or [O O O]
+        :param player: a human or a computer
+        :return: True if the player wins
+        """
         state = self.board.get_board()
         win_state = [
         [state[0][0], state[0][1], state[0][2]],
@@ -87,6 +113,10 @@ class State:
 
 
     def evaluate(self):
+        """
+        Function to heuristic evaluation of state.
+        :return: +1 if the computer wins; -1 if the human wins; 0 draw
+        """
         if self.wins(COMP):
             score = +1
         elif self.wins(HUMAN):
@@ -97,9 +127,21 @@ class State:
         return score
 
     def game_over(self):
+        """
+        This function test if the human or computer wins
+        :return: True if the human or computer wins
+        """
         return self.wins(HUMAN) or self.wins(COMP)
 
     def minimax(self,state, depth, player):
+        """
+        AI function that choice the best move
+        :param state: current state of the board
+        :param depth: node index in the tree (0 <= depth <= 9),
+        but never nine in this case (see iaturn() function)
+        :param player: an human or a computer
+        :return: a list with [the best row, best col, best score]
+        """
         
         if player == COMP:
             best = [-1, -1, -infinity]
@@ -127,6 +169,9 @@ class State:
         return best
         
     def render(self, c_choice, h_choice):
+        """
+        Print the board on console
+        """
         chars = {
             -1: h_choice,
             +1: c_choice,
@@ -142,6 +187,13 @@ class State:
             print('\n' + str_line)
 
     def ai_turn(self , c_choice, h_choice):
+        """
+        It calls the minimax function if the depth < 9,
+        else it choices a random coordinate.
+        :param c_choice: computer's choice X or O
+        :param h_choice: human's choice X or O
+        :return:
+        """
         depth = len(self.board.empty_cells())
         if depth == 0 or self.game_over():
             return
@@ -162,6 +214,12 @@ class State:
         # time.sleep(1)
 
     def human_turn(self,c_choice, h_choice):
+        """
+        The Human plays choosing a valid move.
+        :param c_choice: computer's choice X or O
+        :param h_choice: human's choice X or O
+        :return:
+        """
   
         depth = len(self.board.empty_cells())
         if depth == 0 or self.game_over():
@@ -207,6 +265,9 @@ def clean():
         system('clear')   
         
 def main():
+    """
+    Main function that calls all functions
+    """
     # Paul Lu.  Set the seed to get deterministic behaviour for each run.
     #       Makes it easier for testing and tracing for understanding.
     randomseed(274 + 2020)
